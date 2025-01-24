ceda522c0fdbabb8746a069e7644e84823c08f78
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 <sullemanhossam4@gmail.com>
2025-01-24 11:35:51 +0000

    push

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Install Hugo
-HUGO_VERSION=0.111.2
-ARCH=amd
-wget -O /tmp/hugo.deb https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-${ARCH}64.deb
-dpkg -i /tmp/hugo.deb
-
 # Install Python dependencies
-python3 -m pip install --upgrade pip setuptools wheel
+pip3 install --upgrade pip setuptools wheel
 pip3 install -r requirements.txt
 
-# Build the Hugo project
-hugo --minify
+# Run Hugo build using npx
+npx hugo --minify

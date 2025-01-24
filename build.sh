6228b6577f03ff5cab3ba11aeada100392f3f2b1
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 <sullemanhossam4@gmail.com>
2025-01-24 11:45:32 +0000

    build

--- build.sh
+++ build.sh
@@ -1,32 +1,29 @@
-##!/bin/bash
-#
-## Install Python dependencies
-#pip3 install --upgrade pip setuptools wheel
-#pip3 install -r requirements.txt
-#
-## Run Hugo from node_modules
-#node_modules/.bin/hugo --minify
-
 #!/bin/bash
 
 # Define the Hugo version
 HUGO_VERSION=0.111.2
 
+# Install Node.js dependencies
+echo "Installing Node.js dependencies..."
 npm install
 
 # Download and install Hugo
+echo "Downloading and installing Hugo..."
 curl -L -o /tmp/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar -xzf /tmp/hugo.tar.gz -C /tmp
-
-# Move Hugo binary to a location in PATH
 mv /tmp/hugo /usr/local/bin/hugo
 
 # Verify Hugo installation
+echo "Verifying Hugo installation..."
 hugo version
 
 # Install Python dependencies
+echo "Installing Python dependencies..."
 pip3 install --upgrade pip setuptools wheel
 pip3 install -r requirements.txt
 
 # Build the Hugo site
+echo "Building the Hugo site..."
 hugo --minify
+
+echo "Setup complete!"
